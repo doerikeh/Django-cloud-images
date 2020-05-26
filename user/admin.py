fa64ee0343be7e3e77b793f77a869dfb3be33bee
doerikeh <doerikeh1@gmail.com>
2020-05-26 18:46:48 +0700

    create django template admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as AdminUser
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class Users(AdminUser):
+    fieldsets = (
+        (None, {"fields":("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name",)},),
+        ("Permission", {"fields":("is_active", "is_staff", "is_superuser", "groups", "user_permissions")},),
+        ("Important date", {"fields": ("last_login", "date_joined")},),
+    )
+    
+    add_fieldsets = (
+        (None, {"classes":("wide",),"fields":("email", "password1", "password2"),},),
+    )
+
+
+    list_filter = ("is_staff", "is_superuser", "is_active")
+    list_display_links = ("email",)
+    list_display = ("email","is_staff" , "last_login",)
+    search_fields = ("email", "first_name", "last_name", )
+    ordering = ("email",)
