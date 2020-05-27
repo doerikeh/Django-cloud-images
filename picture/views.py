4b3fa3b6a44934bf2650bc9a9075b5f8cd4a039f
doerikeh <doerikeh1@gmail.com>
2020-05-27 07:08:54 +0700

    create View Picture

--- picture/views.py
+++ picture/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .models import Picture
+from django.views.generic.list import ListView
+from django.shortcuts import get_object_or_404
 
 
-def home(request):
-    return render(request, "home.html", {})
+class PictureList(ListView):
+    model = Picture
+    queryset = Picture.objects.all()
+    context_object_name = "picture_list"
+    template_name = "home.html"
+    paginate_by = 20
