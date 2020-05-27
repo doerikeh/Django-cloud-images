e7d2fe8cd6006417c2248c94aea38a6be2514c95
doerikeh <doerikeh1@gmail.com>
2020-05-27 10:30:55 +0700

    create simple search

--- picture/urls.py
+++ picture/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import PictureList, search
 
 app_name = "picture"
 
 urlpatterns = [
-    path("", home)
+    path("", PictureList.as_view(), name="img-list"),
+    path("search/", search, name="search")
 ]
--- picture/views.py
+++ picture/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Picture
 from django.views.generic.list import ListView
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 
 
 class PictureList(ListView):
@@ -10,3 +11,17 @@ class PictureList(ListView):
     context_object_name = "picture_list"
     template_name = "home.html"
     paginate_by = 20
+
+
+def search(request):
+    if request.method == "GET":
+        pictures = request.GET.get("search", '')
+        try:
+            status = Picture.objects.filter(Q(user__email__icontains=pictures) |
+                                        Q(deskripsi__icontains=pictures))
+        except Picture.DoesNotExist:
+            status = None
+        return render(request, "search.html", {"picture":status})
+    else:
+        return render(request, "search.html",{})
+        
