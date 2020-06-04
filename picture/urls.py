3465a9ee845342560c2d38248c75301b4a46e292
doerikeh <doerikeh1@gmail.com>
2020-06-04 15:33:41 +0700

    create view detail

--- picture/urls.py
+++ picture/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import PictureList, search
+from .views import PictureList,SearchView, upload, PictureDetail, add_or_change_picture, delete_picture
 
 app_name = "picture"
 
 urlpatterns = [
     path("", PictureList.as_view(), name="img-list"),
-    path("search/", search, name="search")
+    path("upload/", upload, name="upload"),
+    path("picture/<pk>/", PictureDetail.as_view(), name="detail"),
+    path("<uuid:pk>/change/", add_or_change_picture, name="change"),
+    path("<uuid:pk>/delete/", delete_picture, name="delete"),
+    path("search/", SearchView.as_view(), name="search")
 ]
