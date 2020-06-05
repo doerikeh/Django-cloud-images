1b7bc0bf6141fbba809a2850aae110de72b6aa08
doerikeh <doerikeh1@gmail.com>
2020-06-05 16:49:30 +0700

    fixes

--- picture/views.py
+++ picture/views.py
@@ -1,10 +1,19 @@
+from datetime import datetime
+import os
+from django.core.files.base import ContentFile
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.utils.translation import ugettext_lazy as _
+from django.views.decorators.csrf import csrf_protect
+from .storage import upload_storage
+
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponseRedirect
 from .models import Picture
 from user.models import Project
 from django.db.models import Q
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, FormView
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -22,6 +31,8 @@ class PictureDetail(DetailView):
     model = Picture
     context_object_name = "picture_detail"
     template_name = "detail_picture.html"
+
+
 
 @login_required
 def change_picture(request, pk=None):
@@ -41,14 +52,14 @@ def change_picture(request, pk=None):
     }
     return render(request, "change_picture.html", context)
 
-@login_required
-def delete_picture(request, pk):
-    picture = get_object_or_404(Picture, pk=pk)
-    if request.method == "POST":
-        picture.delete()
-        return redirect("picture:list")
-    context =  {"picture":picture}
-    return render(request, "picture_delete_confirm.html", context)
+# @login_required
+# def delete_picture(request, pk):
+#     picture = get_object_or_404(Picture, pk=pk)
+#     if request.method == "POST":
+#         picture.delete()
+#         return redirect("picture:list")
+#     context =  {"picture":picture}
+#     return render(request, "picture_delete_confirm.html", context)
     
 
 
@@ -81,15 +92,62 @@ class SearchView(ListView):
             return qs
         return Picture.objects.none()
 
+def _upload_to(request, filename):
+    user = (f"user-{request.use.pk}"
+            if request.user.is_authenticated else "tidal diketahui")
+    return os.path.join("picture", user, f"{datetime.now():%Y/&m/%d}", filename)
 
-def upload(request):
-    if request.method == "POST":
-        form = ImageUpload(request.POST, request.FILES)
-        if form.is_valid():
-            picture = form.save(commit=False)
-            picture.user = request.user
-            picture.save()
-    else:
-        form = ImageUpload()
-    return render(request, "upload_image.html", {"pictures":form})
+@csrf_protect
+def upload_picture(request):
+    status_code = 400
+    data = {
+        "files": [],
+        "error": _("Bad_request")
+    }
+    if request.method == "POST" \
+            and request.is_ajax() \
+            and "picture" in request.FILES:
+        image_typ = [f"image/{x}" for x in [
+            "gif", "jpg", "png", "svg", "jpeg"
+        ]]
+        picture = request.FILES['img']
+        if picture.content_type not in image_typ:
+            status_code = 405
+            data["error"] = _("invalid format picture")
+        else:
+            upload_to = _upload_to(request, picture.name)
+            name = upload_storage.save(upload_to, ContentFile(picture.read()))
+            picture = upload_storage.open(name)
+            status_code = 200
+            del data['error']
+            picture.filename = os.path.basename(picture.name)
+            data["files"].append(picture)
+    json_data = render_to_string("upload.json", data)
+    return HttpResponse(json_data, content_type="application/json", status=status_code)
+
+
+@csrf_protect
+def delete_picture(request, filename):
+    if request.method == "DELETE" \
+            and request.is_ajax() \
+            and filename:
+        try:
+            upload_to = _upload_to(request, filename)
+            upload_storage.delete(upload_to)
+        except FileNotFoundError:
+            pass
+    json = render_to_string("upload.json", {"file":[]})
+    return HttpResponse(json, conten_type="application/json", status=200)
+
+
+# def upload(request):
+#     if request.method == "POST":
+#         form = ImageUpload(request.POST, request.FILES)
+#         if form.is_valid():
+#             picture = form.save(commit=False)
+#             picture.user = request.user
+#             picture.save()
+#     else:
+#         form = ImageUpload()
+#     return render(request, "upload_image.html", {"pictures":form})
         
